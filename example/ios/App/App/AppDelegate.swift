9195dd2cde5607ad0e90c0ee6c45b5d449a96b6e
reytum <Neverfight@619>
2021-04-16 21:29:54 +0530

    add app delegate changes

--- example/ios/App/App/AppDelegate.swift
+++ example/ios/App/App/AppDelegate.swift
@@ -35,6 +35,7 @@ func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive respo
   func applicationDidEnterBackground(_ application: UIApplication) {
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    KommunicateWrapper.shared.applicationDidEnterBackground(application)
   }
 
   func applicationWillEnterForeground(_ application: UIApplication) {
